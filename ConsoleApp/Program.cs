74213c3bbe90eb19dc8e31b56a916fb5b83d6799
Sasidhar Pidikiti <sasidharpidikiti9@gmail.com>
2021-08-26 16:00:59 -0700

    initial import

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add(5, 10);
+            //int c = 0;
+            //c = Add(5, 10);
+            Console.WriteLine("enter a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int c = Add(a, b);
+
+            Console.WriteLine("the sum of  a & b  {0} ", Convert.ToString(c));
+            Console.ReadLine();
+        }
+        public static int Add(int a,int b)
+        {
+            //Console.WriteLine("enter value for a ", a);
+            //Console.ReadLine();
+            //Console.WriteLine("enter value for b ", b);
+            //Console.ReadLine();
+            //Console.WriteLine("The result is ", a+b);
+            //Console.ReadLine();
+            return a + b;
+        }
+    }
+}
---
+++ WebForms/About.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="WebForms.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3>Your application description page 123.</h3>
+    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    <p>Use this area to provide additional information.</p>
+</asp:Content>
